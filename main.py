14ab8f6267a7abe10219dd9a1482bf870b230e49
João Pedro <devcontatojoao@gmail.com>
2025-04-29 23:27:57 -0300

    Start

---
+++ main.py
@@ -0,0 +1,121 @@
+import os
+from time import sleep
+
+users = []
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def menu():
+    clear()
+    dashes = 40
+    dash(dashes)
+    print("Sistema de Cadastro de Usuários".center(dashes))
+    dash(dashes)
+    print("1 - Cadastrar um novo usuário")
+    print("2 - Ver dados cadastrados")
+    print("3 - Sair")
+    dash(dashes)
+    print("Escolha uma opção: ", end="")
+
+
+def get_option():
+    menu()
+    while True:
+        try:
+            option = int(input())
+            if option in [1, 2, 3]:
+                return option
+            else:
+                print("Opção inválida. Tente novamente.")
+                sleep(1)
+                clear()
+                menu()
+        except ValueError:
+            print("Entrada inválida. Por favor, insira um número.")
+            sleep(1)
+            clear()
+            menu()
+
+
+def get_user_data():
+    clear()
+    dash(30)
+    print("Cadastro de Usuário".center(30))
+    dash(30)
+    print()
+
+    user_data = {}
+    user_data["name"] = input("Nome: ")
+    user_data["age"] = input("Idade: ")
+    user_data["email"] = input("Email: ")
+    return user_data
+
+
+def save_user_data():
+    users.append(get_user_data())
+    print()
+    dash(35)
+    print("Usuário cadastrado com sucesso!".center(35))
+    dash(35)
+    sleep(1.5)
+    clear()
+
+
+def show_users():
+    clear()
+    dash(80)
+    print("Usuários Cadastrados".center(80))
+    dash(80)
+    print()
+
+    if users:
+        for i, user in enumerate(users):
+            print("-" * 80)
+            print(
+                f"Usuário {i + 1}: Nome: {user['name']} - Idade: {user['age']} - Email: {user['email']}".center(
+                    80
+                )
+            )
+            if i == len(users) - 1:
+                print("-" * 80)
+    else:
+        dash(30)
+        print("Nenhum usuário cadastrado.".center(30))
+        dash(30)
+    print()
+    input("Pressione Enter para continuar...")
+
+
+def start():
+    while True:
+        option = get_option()
+        if option == 1:
+            save_user_data()
+        elif option == 2:
+            show_users()
+        elif option == 3:
+            clear()
+            dash(30)
+            print("Saindo...".center(30))
+            dash(30)
+            sleep(1)
+            break
+
+
+def dash(num=30):
+    print("=" * num)
+
+
+def main():
+    clear()
+    dash(50)
+    print("Bem-vindo ao sistema de cadastro de usuários!".center(50))
+    dash(50)
+    sleep(2)
+    start()
+
+
+main()
